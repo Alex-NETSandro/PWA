227f3a322dd673267aaf367730a22bd4446c7344
Alex-NETSandro <56177328+Alex-NETSandro@users.noreply.github.com>
2020-02-26 21:14:45 -0300

    Criação de classes e página

---
+++ Blog/Blog/Models/Post/PostEntity.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models.Author;
+using Blog.Models.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Post
+{
+    public class PostEntity
+    {
+        public string Title { get; set; }
+        public AuthorEntity AuthorEntity { get; set; }
+        public CategoryEntity CategoryEntity { get; set; }
+    }
+}
