c860e40b649d088f3b6635b3fb32ab6a28c63366
Alex-NETSandro <56177328+Alex-NETSandro@users.noreply.github.com>
2020-03-06 15:46:10 -0300

    Adicionando dependencias e criando a página

---
+++ Blog/Blog/Models/Post/Review/ReviewEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Post.Review
+{
+    public class ReviewEntity
+    {
+        public PostEntity PostEntity { get; set; }
+        public string Text { get; set; }
+        public int Version { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ Blog/Blog/Models/Tag/TagEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Tag
+{
+    public class TagEntity
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
